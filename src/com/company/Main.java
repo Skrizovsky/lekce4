6a44e573d399ab295fd7fe2b2fb9a237a8f281fd
Skrizovsky <https://github.com/Skrizovsky/ukol1.git>
2022-02-21 20:23:18 +0100

    intial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write SOUtyour code here
         int test = 10;
-        System.out.println("AHOJ"+ test);
+        System.out.println("AHOJ"+" "+test);
     }
 }
